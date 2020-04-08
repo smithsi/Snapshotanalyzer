b43ecc110139db581f7a11ae0de8446b809677bc
smithsi <simoniainsmith@gmail.com>
2020-04-08 17:34:25 +0100

    Updates to shotty.py

--- shotty.py
+++ shotty.py
@@ -1,7 +1,21 @@
 import boto3
+import click
 
-if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.client('ec2')
-    response = ec2.describe_instances()
-    print(response)
+session = boto3.Session(profile_name='shotty')
+ec2 = session.client('ec2')
+
+@click.command()
+def list_instances():
+    "List EC2 instances"
+    for i in ec2.describe_instances():
+        print(','.join((
+        i.id,
+        i.instance_type,
+        i.placement['Availability Zone'],
+        i.state['Name'],
+        i.public_dns_name())))
+    
+    return
+    
+if __name__=='__main__':
+    list_instances()
